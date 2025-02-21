770daaf25bd0005dec357051c5a1db976d09707c
NikolozMamulashvili <mamulashvilinikoloz7@gmail.com>
2025-02-21 23:34:58 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        //FileParserService.CreateTableStyledTXT(3, 2, 5);
         var employeeList = FileParserService.ReadDataFromDB("DATADBCopy.txt");
         FileParserService.CreateTableStyledTXT(employeeList);
     }
